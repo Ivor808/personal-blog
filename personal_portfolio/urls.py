ef35a64c9a52eb0fa3afe6a2ce62106b42fbd6c5
Ivor808 <ivor808@gmail.com>
2019-09-13 18:45:52 -0700

    Initial commit

---
+++ personal_portfolio/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
+    path("", include("projects.urls"))
+]
