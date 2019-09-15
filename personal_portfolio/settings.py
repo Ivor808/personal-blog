072daaa644e4fc9e972a442416b6843dbe64c229
Ivor808 <ivor808@gmail.com>
2019-09-15 15:49:01 -0700

    Fixed images

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'projects',
     'blog',
-    'storages',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -130,9 +130,10 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.s3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,6 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.ImageField(default='project1.png', upload_to='images')
+
+    def __str__(self):
+        return self.title
