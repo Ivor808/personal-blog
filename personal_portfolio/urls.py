68d0c7385e53b6b923c9de285bd052e973c51634
Ivor808 <ivor808@gmail.com>
2021-04-14 09:48:31 -0700

    added aboutme

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("aboutme/", include("aboutme.urls")),
     path("blog/", include("blog.urls")),
     path("", include("projects.urls"))
 ]
